a31488f91e29ea4621ef9b0c820d110cf4965aa3
chanabanana97 <62520653+chanabanana97@users.noreply.github.com>
2021-05-25 18:47:29 +0300

    games by date

--- project/routes/utils/teams_utils.js
+++ project/routes/utils/teams_utils.js
@@ -7,7 +7,23 @@ async function getGamesByTeamID(team_id) {
     const team_games = await DButils.execQuery(
       `select * from dbo.games_db where local_team_id='${team_id}' or visitor_team_id='${team_id}'`
     );
-    return team_games;
+    return splitPastFutureGames(team_games);
   }
+
+async function splitPastFutureGames(team_games) {
+    let curr_date = new Date();
+    let past_games = [];
+    let future_games = [];
+    let all_games = []
+    for(i=0; i < team_games.length; i++){
+        if (team_games[i].game_date < curr_date)
+            past_games.push(team_games[i])
+        else
+            future_games.push(team_games[i])
+    }
+    all_games.push(past_games);
+    all_games.push(future_games);
+    return all_games;
+    }  
 
   exports.getGamesByTeamID = getGamesByTeamID
