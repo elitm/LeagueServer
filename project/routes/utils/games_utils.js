3a22d39c9b10ce9da68411211cb66e1e047d2415
chanabanana97 <62520653+chanabanana97@users.noreply.github.com>
2021-06-14 17:51:25 +0300

    fixes for 3-3 (return game id)

--- project/routes/utils/games_utils.js
+++ project/routes/utils/games_utils.js
@@ -13,8 +13,9 @@ async function getGames(){
   ids.map((id) => events_promises.push(getEventsOfGame(id)));
   let events = await Promise.all(events_promises);
   const games_info = await getGamesInfo(ids);
+  const split_games = await splitPastFutureGames(games_info);
   events = events.filter(item => item.length > 0); // remove empty arrays (games that dont have events)
-  return [games_info, events];
+  return [split_games, events];
 }
 
 async function getGamesInfo(games_ids_list) {
@@ -27,7 +28,7 @@ async function getGamesInfo(games_ids_list) {
   //function for one game query with game_id 
 async function getGame(game_id){
     const q_games = await DButils.execQuery(
-        `select local_team_id, visitor_team_id, game_date, local_team_score, visitor_team_score, field, referee_id from games_db where game_id='${game_id}'`);
+        `select game_id, local_team_id, visitor_team_id, game_date, local_team_score, visitor_team_score, field, referee_id from games_db where game_id='${game_id}'`);
     let GameDetail = await gamesDetails(q_games[0]);
     return GameDetail;    
 }
@@ -39,6 +40,7 @@ async function gamesDetails(q_games){
     let visitor_team_id = q_games.visitor_team_id;
     let referee_id = q_games.referee_id;
 
+    gameWithDetails.game_id = q_games.game_id;
     gameWithDetails.local_team = await teamUtils.getTeamName(local_team_id);
     gameWithDetails.visitor_team = await teamUtils.getTeamName(visitor_team_id);
     gameWithDetails.game_date = q_games.game_date;
@@ -51,9 +53,10 @@ async function gamesDetails(q_games){
 }
 function extractRelevantGameData(games_info) {
     return games_info.map((game_info) => {
-      const {local_team, visitor_team, game_date, local_team_score, visitor_team_score, field, referee} = game_info;
+      const {game_id, local_team, visitor_team, game_date, local_team_score, visitor_team_score, field, referee} = game_info;
       //const { name } = game_info.data.data.team.data;
       return {
+        game_id: game_id,
         local_team: local_team,
         visitor_team: visitor_team,
         game_date: game_date,
